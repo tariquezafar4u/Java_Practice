79ade7e04af614c6ef102f3b340a5ce5250323cf
tariquezafar4u <tariquezafar4u@gmail.com>
2020-08-07 00:55:51 +0530

    push code

--- src/Array_Demo.java
+++ src/Array_Demo.java
@@ -1,7 +1,7 @@
 public class Array_Demo {
 
         public static void main(String args[]) {
-            int arr[] = {10,9,8,7,6,5,3,4,3,2,1,0};
+            int arr[] = {10,9,8,7,6,5,3,4,0,2,1,3};
 
             for (int i = 0; i < arr.length; i++) {
                 for (int j = i+1 ; j < arr.length; j++) {
