ba3a2281731540a4ac7b1d3bf8598813d661d74d
tariquezafar4u <tariquezafar4u@gmail.com>
2020-08-07 00:46:45 +0530

    push code

---
+++ src/Array_Demo.java
@@ -0,0 +1,17 @@
+public class Array_Demo {
+
+        public static void main(String args[]) {
+            int arr[] = {10,9,8,7,6,5,3,4,3,2,1,0};
+
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = i+1 ; j < arr.length; j++) {
+                    if (arr[i] + arr[j] != 3) {
+                        { continue;}
+                    }
+                        System.out.println("Value which totals 3 are: " + arr[i]   + " and " + arr[j] );
+
+
+                }
+            }
+
+        }}
